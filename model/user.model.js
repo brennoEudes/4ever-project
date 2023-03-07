f528d5227db36f42bc614fd8fbe144d8ec7e9a24
“Brenno <brennoeudes@gmail.com>
2023-03-07 00:34:47 -0300

    Criação vault schema

--- model/user.model.js
+++ model/user.model.js
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
 const userSchema = new Schema({
   name: { type: String, required: true, trim: true },
@@ -12,6 +12,7 @@ const userSchema = new Schema({
   passwordHash: { type: String, required: true },
   role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
   createdAt: { type: Date, default: Date.now() },
+  vaults: { type: Types.ObjectsId, ref: "Vault }
 });
 
 export const UserModel = model("User", userSchema);
---
+++ model/vault.model.js
@@ -0,0 +1,18 @@
+
+import { Schema, model, Types } from "mongoose";
+
+const vaultSchema = new Schema({
+  title: { type: String, required: true, trim: true },
+  description: { type: String, maxlength: 100 },
+  receiverInfo: {
+    receiverName: { type: String, required: true, trim: true },
+    receiverEmail: { type: String, required: true, trim: true },
+    receiverPhone: { type: Number, required: true, trim: true },
+  },
+  deliveryDate: { type: Date },
+  message: { type: String, required: true, maxlength: 100000 },
+  attachedDocument: { type: String },
+  titular: { type: Types.ObjectsId, ref: "User" },
+});
+
+export const VaultModel = model("Vault", vaultSchema);
