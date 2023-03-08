0cc83a9cd185efeae68f5893f137db0b569a6907
“Brenno <brennoeudes@gmail.com>
2023-03-08 13:16:37 -0300

    ajustes nomes dos campos em capsuleSchema

--- model/capsule.model.js
+++ model/capsule.model.js
@@ -1,14 +1,14 @@
 import { Schema, model } from "mongoose";
 
 const capsuleSchema = new Schema({
-  title: { type: String, required: true, trim: true },
-  description: { type: String, maxlength: 100 },
-  receiverName: { type: String, required: true, trim: true },
-  receiverEmail: { type: String, required: true, trim: true },
-  receiverPhone: { type: Number, required: true, trim: true },
-  deliveryDate: { type: Date },
-  message: { type: String, required: true, maxlength: 100000 },
-  attachedDocument: { type: String },
+  capsuleTitle: { type: String, required: true, trim: true },
+  capsuleDescription: { type: String, maxlength: 100 },
+  capsuleReceiverName: { type: String, required: true, trim: true },
+  capsuleReceiverEmail: { type: String, required: true, trim: true },
+  capsuleReceiverPhone: { type: Number, required: true, trim: true },
+  capsuleDeliveryDate: { type: Date },
+  capsuleMessage: { type: String, required: true, maxlength: 100000 },
+  capsuleAttachedDocument: { type: String },
   capsuleCreator: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
